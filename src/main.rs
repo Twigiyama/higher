784581a34990c34b98c49c1431ffa0547a69ea62
Asitha Rodrigo <asitharodrigo@Asithas-MacBook-Pro-2023.local>
2023-08-28 21:05:29 +0100

    Random Challenge Done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+use rand::prelude::*;
+
+
 fn main() {
-    println!("Hello, world!");
+    let rand_numb = thread_rng().gen_range(1..101);
+    let mut guess = String::new();
+    let mut number_guess: i32 = 0;
+
+    println!("Test answer is {rand_numb}");
+
+    while number_guess != rand_numb {
+        guess.replace_range(.., "");
+        println!("What is your guess?: ");
+        io::stdin().read_line(&mut guess);
+
+        println!("your guess is {guess}");
+        number_guess = guess.trim().parse().unwrap();
+        if number_guess > rand_numb {
+            println!("{number_guess} is too high");
+        } else if number_guess < rand_numb {
+            println!("{number_guess} is too low");
+        }
+
+    }
+
+    println!("{number_guess} is correct");
+    
+
 }
